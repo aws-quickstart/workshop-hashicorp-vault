eef4a204f5b9df8de907b6b169dcea602e3c55e3
Dan Callao <dcallao@hashicorp.com>
2020-07-20 00:03:02 -0700

    added single vault server tf templates

---
+++ lab_assets/single-vault/iam.tf
@@ -0,0 +1,109 @@
+//--------------------------------------------------------------------
+// Resources
+
+## Vault Server IAM Config
+resource "aws_iam_instance_profile" "vault-server" {
+  name = "${var.stack}-vault-server-instance-profile"
+  role = aws_iam_role.vault-server.name
+}
+
+resource "aws_iam_role" "vault-server" {
+  name               = "${var.stack}-vault-server-role"
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
+
+resource "aws_iam_role_policy" "vault-server" {
+  name   = "${var.stack}-vault-server-role-policy"
+  role   = aws_iam_role.vault-server.id
+  policy = data.aws_iam_policy_document.vault-server.json
+}
+
+# Vault Client IAM Config
+resource "aws_iam_instance_profile" "vault-client" {
+  name = "${var.stack}-vault-client-instance-profile"
+  role = aws_iam_role.vault-client.name
+}
+
+resource "aws_iam_role" "vault-client" {
+  name               = "${var.stack}-vault-client-role"
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
+
+resource "aws_iam_role_policy" "vault-client" {
+  name   = "${var.stack}-vault-client-role-policy"
+  role   = aws_iam_role.vault-client.id
+  policy = data.aws_iam_policy_document.vault-client.json
+}
+
+//--------------------------------------------------------------------
+// Data Sources
+
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    effect  = "Allow"
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+  }
+}
+
+data "aws_iam_policy_document" "vault-server" {
+  statement {
+    sid    = "ConsulAutoJoin"
+    effect = "Allow"
+
+    actions = ["ec2:DescribeInstances"]
+
+    resources = ["*"]
+  }
+
+  statement {
+    sid    = "VaultAWSAuthMethod"
+    effect = "Allow"
+    actions = [
+      "ec2:DescribeInstances",
+      "iam:GetInstanceProfile",
+      "iam:GetRole",
+    ]
+    resources = ["*"]
+  }
+
+  statement {
+    sid    = "VaultKMSUnseal"
+    effect = "Allow"
+
+    actions = [
+      "kms:Encrypt",
+      "kms:Decrypt",
+      "kms:DescribeKey",
+    ]
+
+    resources = ["*"]
+  }
+  statement {
+    sid    = "VaultSecretsManager"
+    effect = "Allow"
+
+    actions = [
+      "secretsmanager:PutSecretValue",
+      "secretsmanager:CreateSecret",
+      "secretsmanager:UpdateSecret"
+    ]
+
+    resources = ["*"]
+  }
+}
+
+data "aws_iam_policy_document" "vault-client" {
+  statement {
+    sid    = "ConsulAutoJoin"
+    effect = "Allow"
+
+    actions = ["ec2:DescribeInstances"]
+
+    resources = ["*"]
+  }
+}
---
+++ lab_assets/single-vault/security-groups.tf
@@ -0,0 +1,131 @@
+# ---------------------------------------------------------------------------------------------------------------------
+# SECURITY GROUP FOR VAULT CLIENT(WEB SERVER)
+# ---------------------------------------------------------------------------------------------------------------------
+resource "aws_security_group" "vault-client_sg" {
+  name   = "vault-client_sg"
+  vpc_id = aws_vpc.main.id
+
+  # SSH
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Vault API traffic
+  ingress {
+    from_port   = 8200
+    to_port     = 8200
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Admin URL
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Internal Traffic
+  ingress {
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
+    self      = true
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name    = "${var.stack}-vault-client-sg"
+    Project = var.stack
+  }
+}
+
+# ---------------------------------------------------------------------------------------------------------------------
+# SECURITY GROUP FOR VAULT SERVER
+# ---------------------------------------------------------------------------------------------------------------------
+resource "aws_security_group" "vault-server_sg" {
+  name   = "vault-server_sg"
+  vpc_id = aws_vpc.main.id
+
+  # SSH
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Vault API traffic
+  ingress {
+    from_port   = 8200
+    to_port     = 8200
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Internal Traffic
+  ingress {
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
+    self      = true
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name    = "${var.stack}-vault-server-sg"
+    Project = var.stack
+  }
+}
+
+# ---------------------------------------------------------------------------------------------------------------------
+# SECURITY GROUP FOR RDS
+# ---------------------------------------------------------------------------------------------------------------------
+resource "aws_security_group" "db-sg" {
+  name        = "${var.stack}-db-sg"
+  description = "Access to the RDS instances from the VPC"
+  vpc_id      = aws_vpc.main.id
+  
+
+  ingress {
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = [var.vpc_cidr]
+  }
+
+  ingress {
+    from_port   = 8
+    to_port     = 0
+    protocol    = "icmp"
+    cidr_blocks = [var.vpc_cidr]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.stack}-db-sg"
+  }
+}
+
